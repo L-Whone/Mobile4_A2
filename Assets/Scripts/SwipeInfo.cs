d9cda8fb14cd2b67df83a13dc4a6527716eb4666
Vi Tran <bibo06@icloud.com>
2026-06-22 22:37:30 -0700

    ADDED: Swipe.cs from last Mobile Assignment

---
+++ Assets/Scripts/SwipeInfo.cs
@@ -0,0 +1,6 @@
+﻿public struct SwipeInfo
+{
+    public float Angle;        // radians
+    public float AngleDegrees; // 0-360
+    public float Magnitude;    // swipeDelta.magnitude / MagnitudeDivider
+}
