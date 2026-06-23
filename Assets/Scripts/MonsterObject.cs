8124ed537a37c5b2fd4f921041f07265a38353ae
Vi Tran <bibo06@icloud.com>
2026-06-22 22:43:31 -0700

    ADDED: MonsterObject,cs

---
+++ Assets/Scripts/MonsterObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MonsterObject : MonoBehaviour
+{
+    public event System.Action<MonsterObject> OnCaptured;
+
+    private bool _isDone = false;
+
+    public void Capture()
+    {
+        if (_isDone) return;
+        _isDone = true;
+        OnCaptured?.Invoke(this);
+        Destroy(gameObject);
+    }
+}
